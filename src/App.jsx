180d5fa9bb1696ced2497f6b94a4f9e829f74516
fady jan <fady.jan95@gmail.com>
2024-08-11 18:23:56 +0300

    Test backgroundImage

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ backgroundImage: `url(/MyImg.jpeg)` }}>
+      <h1>Hello Test background image</h1>
+    </div>
+  );
+}
+
+export default App;
