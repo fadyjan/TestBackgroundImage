fcfb8a86fde48d8e0b3708a76fd8e01718acc266
fady jan <fady.jan95@gmail.com>
2024-08-11 18:35:52 +0300

    Changing header text

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div style={{ backgroundImage: `url(./public/MyImg.jpeg)` }}>
-      <h1>Hello Test background image</h1>
+    <div style={{ backgroundImage: `url(/assets/MyImg.jpeg)` }}>
+      <h1>Hello Changes</h1>
     </div>
   );
 }
