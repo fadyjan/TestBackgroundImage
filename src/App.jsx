ea61264b07109de9252d613f9900bfa1a4001320
fady jan <fady.jan95@gmail.com>
2024-08-11 18:27:36 +0300

    change to public path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div style={{ backgroundImage: `url(/MyImg.jpeg)` }}>
+    <div style={{ backgroundImage: `url(./public/MyImg.jpeg)` }}>
       <h1>Hello Test background image</h1>
     </div>
   );
